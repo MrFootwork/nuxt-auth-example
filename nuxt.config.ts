1cc0c15aa0ea3136147b1021d91680b42efd17b8
Pandau <66787998+MrFootwork@users.noreply.github.com>
2024-07-16 05:09:57 +0200

    Update nuxt.config.ts for cloudflare test
    https://nuxt.com/deploy/cloudflare

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+	nitro: {
+		prerender: {
+			autoSubfolderIndex: false
+		}
+  	}
 	css: ['@fortawesome/fontawesome-free/css/all.min.css'],
 	modules: ['@nuxtjs/tailwindcss', '@sidebase/nuxt-auth'],
 	auth: {
